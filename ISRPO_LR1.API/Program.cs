ab8cb2037fd06a26af7de176412cb8e750da3c1d
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-09-22 21:51:34 +0300

    Добавлены конвертеры Json

--- ISRPO_LR1.API/Program.cs
+++ ISRPO_LR1.API/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ISRPO_LR1.API.Utils;
 using ISRPO_LR1.Domain.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +16,15 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddControllers().AddJsonOptions(opt =>
+{
+    opt.JsonSerializerOptions.Converters.Add(new JsonDateTimeConverter());
+    opt.JsonSerializerOptions.DefaultIgnoreCondition =
+        JsonIgnoreCondition.WhenWritingNull;
+    opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    opt.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
